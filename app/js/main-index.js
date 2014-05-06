c5b6f9b2b0e4789a82222b4c46badd21ab2d8b5e
Bjoern Ganslandt <bjoern.ganslandt@intuio.at>
2014-05-06 13:16:10 +0200

    Remove demo-functionality
    - remove demo-files, as they are being moved to yeoman-generator
    - remove r.js, as it is part of grunt-requirejs

--- app/js/main-index.js
+++
@@ -1,9 +0,0 @@
-
-require(/* ["include", "modules"], */ function(/* Include, Modules */)
-{
-	// Initialize your page specific modules
-	// Include.init();
-	// Modules.init();
-
-	// Add more page specific code
-});
